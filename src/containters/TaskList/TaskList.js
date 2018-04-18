e8f4283f0d741e27b8088cb8781130bb9808169a
tijanadjorovic <tijana89djorovic@gmail.com>
2018-04-18 20:15:43 +0200

    add fetch

--- src/containters/TaskList/TaskList.js
+++ src/containters/TaskList/TaskList.js
@@ -2,13 +2,40 @@ import React, {Component} from 'react';
 import './TaskList.css';
 
 class TaskList extends Component{
-    render(){
-        return(
-            <div>
-                <p>TaskList</p>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
     }
+  }
+
+  componentDidMount() {
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.slice(0,10));
+        this.setState({todos: data});
+        });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Task List</h1>
+        {
+          this.state.todos.slice(0,10).map(
+              todo => <div
+                  key={todo.id}>
+                  <div >
+                    {todo.title}
+                  </div>
+                  </div>
+          )
+        }
+      </div>
+    )    
+  }
 }
 
 export default TaskList;
