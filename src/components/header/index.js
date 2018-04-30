b1af728e3ebbdb6f278468f34e85675dbfe7948e
tijanadjorovic <tijana89djorovic@gmail.com>
2018-04-30 16:40:05 +0200

    fix header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,23 +3,13 @@ import { Link } from "react-router-dom";
 
 
 const Header = (props) => {
-    return (
-        <div  id="top">
-            <ul >
-                {!props.redirectToReferrer && <span>
-                    <li >
-                        <Link to="/login">Login</Link>
-                    </li>
-                </span>}
-
-                {props.redirectToReferrer && <span>
-                    <li >
-                        <a onClick={() => props.handleSignOut()}>Sign Out</a>
-                    </li>
-                </span>}
-            </ul>
-        </div>
-    )
+  return (
+    <div id="top">
+      {props.redirectToReferrer && <span>  
+        <button onClick={() => props.handleSignOut()}>Log Out</button>
+      </span>}
+    </div>
+  )
 };
 
 export default Header;
